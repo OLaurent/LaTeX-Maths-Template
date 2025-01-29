8f7f0d62118a9f3da549ecbc25765b76da155252
Olivier Laurent <olivier-xavier.laurent@ac-toulouse.fr>
2025-01-29 19:25:32 +0000

    updated packages

--- scripts/initialize-dependencies.sh
+++ scripts/initialize-dependencies.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install texlive-xetex
+
+sudo cp ./texmf/math-vh.sty /usr/local/texlive/2024/texmf-dist/tex/latex
+sudo texhash
+
+
+sudo tlmgr install ntheorem
+sudo tlmgr install pgf
+sudo tlmgr install tcolorbox
+sudo tlmgr install framed
